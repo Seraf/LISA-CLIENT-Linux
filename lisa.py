da10048e99524fbfbae80dab5205e1a514ecbdfc
Julien Syx <julien.syx@gmail.com>
2013-12-21 00:41:37 +0100

    Adding speech dispatcher to lisa client to have multi room output. Need some tests on raspberry

--- lisa.py
+++ lisa.py
@@ -1,32 +1,43 @@
 from twisted.internet import ssl, reactor
 from twisted.internet.protocol import Protocol, ReconnectingClientFactory
 from twisted.protocols.basic import LineReceiver
+from twisted.application import internet, service
 from twisted.python import log
 import json, os
 from OpenSSL import SSL
+import speechd
 
 path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
-configuration = json.load(open(os.path.normpath(dir_path + '/' + 'Configuration/lisa.json')))
+configuration = json.load(open(os.path.normpath(dir_path + '/' + 'configuration/lisa.json')))
+
+client = speechd.Speaker('LISA')
+client.set_punctuation(speechd.PunctuationMode.SOME)
+client.set_output_module(str(configuration['tts']))
+client.set_language(str(configuration['lang']))
 
 class LisaClient(LineReceiver):
     def __init__(self,factory):
         self.factory = factory
 
-    def sendMessage(self, message):
+    def sendMessage(self, message, type='chat'):
+        if configuration['debug']['debug_output']:
+            log.msg('"from": "Linux","type": type, "body": ' + unicode(message) + ', "zone": '+ configuration['zone'])
         self.sendLine(json.dumps(
-            {"from": 'Linux',"type": 'Speech', "body": unicode(message), "zone": "Android"})
+            {"from": 'Linux',"type": type, "body": unicode(message), "zone": configuration['zone']})
         )
     def lineReceived(self, data):
-        log.msg("data received")
         datajson = json.loads(data)
-        log.msg(datajson)
+        if configuration['debug']['debug_input']:
+            log.msg("Data received :" + str(datajson))
+        client.speak(str(datajson['body']))
 
     def connectionMade(self):
         log.msg('Connected to Lisa.')
         if configuration['enable_secure_mode']:
             ctx = ClientTLSContext()
             self.transport.startTLS(ctx, self.factory)
+        self.sendMessage('Client connected from ' + configuration['zone'], 'command')
 
 class LisaClientFactory(ReconnectingClientFactory):
     def startedConnecting(self, connector):
@@ -40,6 +51,7 @@ class LisaClientFactory(ReconnectingClientFactory):
 
     def clientConnectionLost(self, connector, reason):
         log.err('Lost connection.  Reason:', reason)
+        client.close()
         ReconnectingClientFactory.clientConnectionLost(self, connector, reason)
 
     def clientConnectionFailed(self, connector, reason):
@@ -55,18 +67,15 @@ class CtxFactory(ssl.ClientContextFactory):
     def getContext(self):
         self.method = SSL.SSLv23_METHOD
         ctx = ssl.ClientContextFactory.getContext(self)
-        ctx.use_certificate_file(os.path.normpath(dir_path + '/' + 'Configuration/ssl/client.crt'))
-        ctx.use_privatekey_file(os.path.normpath(dir_path + '/' + 'Configuration/ssl//client.key'))
+        ctx.use_certificate_file(os.path.normpath(dir_path + '/' + 'configuration/ssl/client.crt'))
+        ctx.use_privatekey_file(os.path.normpath(dir_path + '/' + 'configuration/ssl//client.key'))
 
         return ctx
 
-if __name__ == "__main__":
-    # Start as a regular Python script.
-    LisaFactory = LisaClientFactory()
-
-    if configuration['enable_secure_mode']:
-        reactor.connectSSL(configuration['lisa_url'], configuration['lisa_engine_port_ssl'], LisaFactory, CtxFactory())
-    else:
-        lisaclient = reactor.connectTCP(configuration['lisa_url'], configuration['lisa_engine_port'], LisaFactory)
-
-    reactor.run()
+LisaFactory = LisaClientFactory()
+application = service.Application("LISA-Client")
+if configuration['enable_secure_mode']:
+    lisaclientService =  internet.TCPClient(configuration['lisa_url'], configuration['lisa_engine_port_ssl'], LisaFactory, CtxFactory())
+else:
+    lisaclientService =  internet.TCPClient(configuration['lisa_url'], configuration['lisa_engine_port'], LisaFactory)
+lisaclientService.setServiceParent(application)
