3e8dd86bad18c776aaa3056211f530d345fa60c3
Julien Syx <julien.syx@gmail.com>
2014-03-30 17:00:22 +0200

    Client is working, it seems not too bad :D

--- lib/recorder.py
+++ lib/recorder.py
@@ -1,78 +1,72 @@
-import os
+import alsaaudio
+import wave
 
-import pygtk
-pygtk.require('2.0')
-import gobject
-import pygst
-pygst.require('0.10')
-gobject.threads_init()
-import gst
+class Loader():
+    def __init__(self, end, every):
+        self.end = end / every - 1
+        self.every = every
+        self.progress = '.'
+
+    def _clear_line(self):
+        ''' Clear one terminal line '''
+        print '\x1b[1A',
+
+    def __call__(self, i):
+        ''' Print loader for the ith time '''
+        if not i % self.every:
+            return
+        self._clear_line()
+        print '[Recording]' + self.progress * (i/2) + ' ' * (self.end - (i/2)) + '[Recording]'
 
 
 class Recorder:
-    def __init__(self, listener,vader):
+    def __init__(self, listener, configuration):
         self.listener = listener
-        self.started = False
-        self.finished = False
+        open(self.listener.recordingfile, 'w').write('')
+        self.configuration = configuration
 
-        self.pipeline = self.listener.get_pipeline()
-        self.recording = self.listener.get_wav_file_location()
+        # Microphone stream config.
+        self.chunk = 1024  # CHUNKS of bytes to read each time from mic
+        self.channels = 1
+        self.rate = 16000
 
-        vader.connect('vader_start', self.__start__)
-        vader.connect('vader_stop', self.__stop__)
+        self.RECORD_SECONDS = configuration['record_seconds']
+        self.SLEEP_TIME = configuration['sleep_time']
 
-        bus = self.pipeline.get_bus()
-        bus.add_signal_watch()
-        bus.connect('message::application', self.__application_message__)
+    def listen_for_speech(self):
+        """
+        Listens to Microphone, extracts phrases from it and sends it to
+        Wit speech service
+        """
+        self.write_wav(self.capture_audio(Loader(self.SLEEP_TIME * self.RECORD_SECONDS, 2)))
 
-        print " * Listening closely..."
-        self.listener.recording_state = True
-        gobject.timeout_add_seconds(5, self.cancel)
-        self.start()
-
-    def start(self):
-        self.started = True
-        print " * Recording..."
-
-    def stop(self):
-        print " # stop_now"
-        print " * Stored recording to ", self.recording
-        print " * Done."
-        self.finished = True
         self.listener.answer()
 
-    def cancel(self):
-        print " # cancel", self.finished, self.started
-        if self.finished:
-            print " # cancel - noop"
-            return
+    def capture_audio(self,loader):
+        def setup_mic():
+            inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL)
+            inp.setchannels(self.channels)
+            inp.setrate(self.rate)
+            inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+            inp.setperiodsize(self.chunk)
+            return inp
 
-        if not self.started:
-            self.finished = True
-            print " * Not a word in the past 5 seconds, cancelling"
-            #self.pipeline.set_state(gst.STATE_NULL)
-            #self.recorder.set_state(gst.STATE_NULL)
-            self.listener.cancel_listening()
+        inp = setup_mic()
+        sound = []
 
-    def __start__(self, vader, arg0):
-        if not self.finished:
-            print " # vader:start"
-            struct = gst.Structure('vader_start')
-            struct.set_value('arg0', arg0)
-            vader.post_message(gst.message_new_application(vader, struct))
+        print "\n[*]> Starting Recording\n"
+        for i in xrange(0, self.rate / self.chunk * self.RECORD_SECONDS):
+            loader(i)
+            _, data = inp.read()
+            sound.append(data)
+        print "[*]> Ready Recognize Voice\n"
 
-    def __stop__(self, vader, arg0):
-        if not self.finished:
-            print " # vader:stop"
-            struct = gst.Structure('vader_stop')
-            struct.set_value('arg0', arg0)
-            vader.post_message(gst.message_new_application(vader, struct))
+        return ''.join(sound)
 
-    def __application_message__(self, bus, msg):
-        msgtype = msg.structure.get_name()
-        if self.finished:
-            return
-        if msgtype == 'vader_stop':
-            self.stop()
-        elif msgtype == 'vader_start':
-            self.start()
+    def write_wav(self, data):
+        wf = wave.open(self.listener.recordingfile, 'wb')
+        wf.setnchannels(self.channels)
+        wf.setsampwidth(2)
+        wf.setframerate(self.rate)
+        wf.writeframes(data)
+        wf.close()
