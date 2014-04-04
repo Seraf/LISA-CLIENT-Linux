15445ef8ad897e494985cfcbed1ec084df4cdf34
Julien Syx <julien.syx@gmail.com>
2014-04-04 20:56:06 +0200

    Moving lisa.json to sample file to avoid conflict/merging problem. Install.sh copy this file to have something quickly working

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt-get install python-dev build-essential gstreamer0.10-plugins-good python-gobject python-gi gstreamer0.10-pocketsphinx gstreamer0.10-alsa
 sudo pip install -r requirements.txt
-
+cp lisa/configuration/lisa.json.sample lisa/configuration/lisa.json
