9b5c7b5febf6361b796640eed65334ed0a33ff9e
Julien Syx <julien.syx@gmail.com>
2014-04-04 15:49:27 +0200

    Adding dependancies

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo apt-get install python-dev build-essential gstreamer0.10-plugins-good python-gobject
+sudo apt-get install python-dev build-essential gstreamer0.10-plugins-good python-gobject python-gi gstreamer0.10-pocketsphinx gstreamer0.10-alsa
 sudo pip install -r requirements.txt
-#sudo dpkg -i package/libttspico0_1.0+git20110131-2_armhf.deb package/libttspico-data_1.0+git20110131-2_all.deb package/libttspico-dev_1.0+git20110131-2_armhf.deb package/libttspico-utils_1.0+git20110131-2_armhf.deb
 
